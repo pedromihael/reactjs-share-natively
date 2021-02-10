e7037ddd00c4202437809ae88202a362087c1283
Pedro Mihael <pedromvpaes@gmail.com>
2021-02-09 22:00:12 -0400

    chore: add base files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback } from 'react';
 
 function App() {
+  const handleShare = useCallback(async () => {
+    const filesArray = ['src/assets/test.mp4'];
+
+    if (navigator.canShare && navigator.canShare({ files: filesArray })) {
+      navigator
+        .share({
+          files: filesArray,
+          title: 'Video sharing test',
+          text: 'test',
+        })
+        .then(() => console.log('Share was successful.'))
+        .catch((error) => console.log('Sharing failed', error));
+    } else {
+      console.log(`Your system doesn't support sharing files.`);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleShare}>share</button>
+      <p className='result'></p>
+    </>
   );
 }
 
